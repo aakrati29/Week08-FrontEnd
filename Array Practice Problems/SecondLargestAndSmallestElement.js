6f02f5918a2844259bba59bd0bb8c143be36f14c
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-03-03 15:31:55 +0530

    [Aakrati] add. Find second largest and second smallest element done

---
+++ Array Practice Problems/SecondLargestAndSmallestElement.js
@@ -0,0 +1,34 @@
+let number = [];
+    for(let i = 0; i < 10; i++){
+        let ramdomThreeDigitNum = Math.floor(Math.random() * 990) + 100;
+        number.push(ramdomThreeDigitNum);
+    }
+
+    console.log("Ten random numbers are : " + number);
+
+    let min = Infinity;
+    let secondMin = Infinity;
+    let max = -Infinity;
+    let secondMax = -Infinity;
+
+   for(let num of number){
+    if(num < min){
+        secondMin = min;
+        min = num;
+    }
+    else if(num < secondMin && num !== min){
+        secondMin = num;
+    }
+
+    if(num > max){
+        secondMax = max;
+        max = num;
+    }
+
+    else if(num > secondMax && num !== max){
+        secondMax = num;
+    }
+   }
+
+   console.log("Second maximum is : " + secondMax);
+   console.log("Second minimum is : " + secondMin);
