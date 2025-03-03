45ad7207f3ff0569beed81e19fc2886bacf23ee5
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-03-03 11:47:07 +0530

    [Aakrati] add. Number to weel days

---
+++ Selection Practice Problems with if else if and else/NumberToWeekDays.js
@@ -0,0 +1,23 @@
+let number = parseInt(process.argv[2]);
+
+if(number === 1){
+    console.log("Monday");
+}
+else if(number === 2){
+    console.log("Tuesday");
+}
+else if(number === 3){
+    console.log("Wednesday");
+}
+else if(number === 4){
+    console.log("Thrusday");
+}
+else if(number === 5){
+    console.log("Friday");
+}
+else if(number === 6){
+    console.log("Saturday");
+}
+else{
+    console.log("Sunday");
+}
